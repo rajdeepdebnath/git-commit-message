83283909c3f7daedc4e84feb0fd4929fa7760095
Rajdeep Debnath <debnath.rajdeep@gmail.com>
2020-04-27 05:55:56 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace git_commit_message
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
